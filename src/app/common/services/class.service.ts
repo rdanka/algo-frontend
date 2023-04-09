41c3c716dd81b44a172db0a1a231be6b64dd682a
DESKTOP-AFFH5NC\Robi <lobika2001@gmail.com>
2023-04-09 13:25:32 +0200

    Service call fixed for adding classes

--- src/app/common/services/class.service.ts
+++ src/app/common/services/class.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../enviroment';
 
@@ -10,7 +10,12 @@ export class ClassService {
   constructor(private readonly http: HttpClient) { }
 
   createClass(params: any) {
-    return this.http.post<any>(`${environment.baseUrl}/class/create`, params);
+
+  const headers= new HttpHeaders()
+                .set('content-type', 'application/json')
+                .set('Authorization', `${localStorage.getItem('id_token')}`);
+
+    return this.http.post<any>(`${environment.baseUrl}/users/addClass`, params, { headers });
   }
 
   deleteClass() {}
